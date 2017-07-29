17af2263e7dcd0d6386e70b6ab7cd0d078fb6ffb
Matthew Benton <m@mattb.tech>
2017-07-29 11:28:59 -0700

    First attempt at a setup script

---
+++ galliumos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Stop xscreensaver from beginning on startup and causing the need to enter password when the laptop lid is reopened
+sudo rm /etc/xdg/autostart/xscreensaver.desktop
+# Install flash
+sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+sudo apt-get update
+sudo apt-get install adobe-flashplugin
+echo "Yipee! It's Done!"
