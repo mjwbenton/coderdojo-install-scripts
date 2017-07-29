e1602b7078cde29b48120609810dfa81152fb62f
Matthew Benton <m@mattb.tech>
2017-07-29 11:51:58 -0700

    Polish up a bit and install more stuff

--- galliumos.sh
+++ galliumos.sh
@@ -1,7 +1,21 @@
+# This setup script:
+# 1. Kills the screensaver and stops it from requiring a password every time the screen locks
+# 2. Updates everything
+# 3. Installs flash
+# 4. Installs some text editors
+
 # Stop xscreensaver from beginning on startup and causing the need to enter password when the laptop lid is reopened
-sudo rm /etc/xdg/autostart/xscreensaver.desktop
-# Install flash
-sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
-sudo apt-get update
-sudo apt-get install adobe-flashplugin
+killall xscreensaver
+sudo su -
+rm /etc/xdg/autostart/xscreensaver.desktop
+
+# Add repo ready to install flash and upgrade packages
+add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+yes | apt-get update
+yes | apt-get upgrade
+
+# Install flash and vim
+yes | apt-get install adobe-flashplugin vim gedit
+
+echo "================================"
 echo "Yipee! It's Done!"
